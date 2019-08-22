97ffa203956f7ba6a954305a07b8ae7b8fbf36ae
Thacen <34222930+Thacen@users.noreply.github.com>
2019-08-22 18:53:30 +0200

    Add whole guide

--- Thacen_30-30.lua
+++ Thacen_30-30.lua
@@ -5,8 +5,8 @@ Guidelime.registerGuide(
 [D A guide created by T H A C E N#9446\Based on Joana's Speed Leveling guide https://www.joanasworld.com]
 01) Follow the path all the way to Hammerfall and accept "[QA655 Hammerfall]" (down by the Forge, [G73,34] in Arathi Highlands).
 02) SE at Hammerfall, turn in "[QT655 Hammerfall]" ([G75,36]), accept "[QA672 Raising Spirits]".
-03) Go up the steps and get the FP from the Wind Rider Master ([G73,33]).  Make sure you are repaired/resupplied.
-04) Just to the west of Hammerfall (around [G70,35]), do "[QC672 Raising Spirits]": kill/loot Highland Striders (raptors).  80% drop rate.
+03) Go up the steps and get the FP from the Wind Rider Master ([G73,33]). Make sure you are repaired/resupplied.
+04) Just to the west of Hammerfall (around [G70,35]), do "[QC672 Raising Spirits]": kill/loot Highland Striders (raptors). 80% drop rate.
 05) Go back to Hammerfall (at Tor'gan, #2 on map) and turn in "[QT672 Raising Spirits]" ([G75,36]), accept the next [QA674 part].
 06) Down in Hammerfall, turn in "[QT674 Raising Spirits]" ([G73,34], #1 on map), accept the next [QA675 part].
 07) Go back to Tor'gan and turn in "[QT675 Raising Spirits]" ([G75,36], #2 on map), SKIP "Guile of the Raptor" (for now).
